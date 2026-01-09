e01836e399d3d4ca6af0829a734da75cb0991299
Kavia AI <production@kavia.ai>
2026-01-09 18:25:33 +0000

    Checkpoint 5 - cg4211ff3e

--- ott_frontend/lib/app.dart
+++ ott_frontend/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ott_frontend/core/i18n/app_localizations.dart';
 import 'package:ott_frontend/core/routing/app_router.dart';
@@ -9,6 +10,7 @@ import 'package:ott_frontend/features/home/controllers/home_controller.dart';
 import 'package:ott_frontend/features/profile/controllers/settings_controller.dart';
 import 'package:ott_frontend/features/search/controllers/search_controller.dart';
 import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 class StreamEaseApp extends StatelessWidget {
   const StreamEaseApp({super.key, required this.deps});
